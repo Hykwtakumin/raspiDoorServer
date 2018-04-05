663a1abc8eb14dfc81275891175ccda6dc058eab
hykwtakumin <hykwtakumin@gmail.com>
2018-04-05 17:28:04 +0900

    ・GPIOポートを変更

--- src/servotest4.js
+++ src/servotest4.js
@@ -2,7 +2,7 @@ var raspi = require('raspi');
 var raspiPwm = require('raspi-pwm');
 
 raspi.init(function() {
-    var pwm = new raspiPwm.PWM('GPIO17');
+    var pwm = new raspiPwm.PWM('GPIO18');
     pwm.write(70);
     while(1);
 })
