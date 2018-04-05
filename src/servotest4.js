b51437e90fdca2b29d6e4591e4d8b0bba4cdf505
hykwtakumin <hykwtakumin@gmail.com>
2018-04-05 17:25:23 +0900

    ・servotest4.jsを修正

--- src/servotest4.js
+++ src/servotest4.js
@@ -1,8 +1,8 @@
 var raspi = require('raspi');
-var PWM = require('raspi-pwm');
+var raspiPwm = require('raspi-pwm');
 
 raspi.init(function() {
-    var pwm = new PWM('GPIO17');
+    var pwm = new raspiPwm.PWM('GPIO17');
     pwm.write(70);
     while(1);
 })
